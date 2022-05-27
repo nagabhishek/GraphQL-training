8d62215eb33fa7863c25ff4ecb76017ab8cd90e2
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-27 15:54:56 +0530

    1 file modified

--- schema/schema.js
+++ schema/schema.js
@@ -7,62 +7,97 @@ const {
   GraphQLID,
   GraphQLList,
   GraphQLInt,
+  GraphQLBoolean,
 } = graphql;
 
 //dummy data
-var booksList = [
+var courseList = [
   {
-    id: '1',
-    name: 'Wings Of Fire',
-    category: 'Autobiography',
-    authorId: '1',
+    id: 1,
+    title: 'React',
+    price: 5000,
+    likes: 400,
+    rating: 5,
+    imageUrl:
+      'https://ms314006.github.io/static/b7a8f321b0bbc07ca9b9d22a7a505ed5/97b31/React.jpg',
+    trainerId: 1,
   },
   {
-    id: '2',
-    name: 'India 2020',
-    category: 'Inspirational',
-    authorId: '1',
+    id: 2,
+    title: 'Redux',
+    price: 4000,
+    likes: 600,
+    rating: 5,
+    imageUrl: 'https://logicalidea.co/wp-content/uploads/2020/05/Redux.jpg',
+    trainerId: 1,
   },
   {
-    id: '3',
-    name: 'Mrutyunjay',
-    category: 'Inspirational',
-    authorId: '2',
+    id: 3,
+    title: 'Node',
+    price: 6000,
+    likes: 900,
+    rating: 4,
+    imageUrl:
+      'https://www.cloudsavvyit.com/p/uploads/2019/07/2350564e.png?width=1198&trim=1,1&bg-color=000&pad=1,1',
+    trainerId: 2,
+  },
+  {
+    id: 4,
+    title: 'Angular',
+    price: 5000,
+    likes: 200,
+    rating: 3,
+    imageUrl:
+      'https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/15991/top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png',
+    trainerId: 3,
+  },
+  {
+    id: 5,
+    title: 'Flutter',
+    price: 7000,
+    likes: 700,
+    rating: 4,
+    imageUrl: 'https://miro.medium.com/max/2000/1*PCKC8Ufml-wvb9Vjj3aaWw.jpeg',
+    trainerId: 1,
   },
 ];
 
-var authorsList = [
-  { id: '1', name: 'Dr. APJ Abdul Kalam', age: 70 },
-  { id: '2', name: 'Shivaji Sawant', age: 75 },
-  { id: '3', name: 'Ranjit Desai', age: 50 },
+var trainersList = [
+  { id: 1, name: 'Sumeet Wajpe', age: 38, isMCT: true },
+  { id: 2, name: 'Gulshan Sachdev', age: 45, isMCT: false },
+  { id: 3, name: 'Rachana Ranade', age: 32, isMCT: true },
 ];
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
+const CourseType = new GraphQLObjectType({
+  name: 'Course',
   fields: () => ({
     id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    category: { type: GraphQLString },
-    author: {
-      type: AuthorType,
+    title: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    rating: { type: GraphQLInt },
+    rating: { type: GraphQLInt },
+    likes: { type: GraphQLInt },
+    imageUrl: { type: GraphQLString },
+    trainer: {
+      type: TrainerType,
       resolve(parent, args) {
-        //console.log(parent);
-        return authorsList.find((a) => a.id == parent.authorId);
+        return trainersList.find((t) => t.id == parent.trainerId);
       },
     },
   }),
 });
 
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
+const TrainerType = new GraphQLObjectType({
+  name: 'Trainer',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
-    books: {
-      type: new GraphQLList(BookType),
+    isMCT: { type: GraphQLBoolean },
+    courses: {
+      type: new GraphQLList(CourseType),
       resolve(parent, args) {
-        return booksList.filter((book) => book.authorId === parent.id);
+        return courseList.filter((course) => course.trainerId === parent.id);
       },
     },
   }),
@@ -71,28 +106,32 @@ const AuthorType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    book: {
-      type: BookType,
+    course: {
+      type: CourseType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data (from database)
-        // console.log(typeof args.id);
-        return booksList.find((b) => b.id == args.id);
+        return courseList.find((c) => c.id == args.id);
       },
     },
-    books: {
-      type: new GraphQLList(BookType),
+    courses: {
+      type: new GraphQLList(CourseType),
       resolve(parent, args) {
-        return booksList;
+        return courseList;
       },
     },
-    author: {
-      type: AuthorType,
+    trainer: {
+      type: TrainerType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data (from database)
-        // console.log(typeof args.id);
-        return authorsList.find((a) => a.id == args.id);
+        return trainersList.find((t) => t.id == args.id);
+      },
+    },
+    trainers: {
+      type: new GraphQLList(TrainerType),
+      resolve(parent, args) {
+        return trainersList;
       },
     },
   },
@@ -137,6 +176,17 @@ module.exports = new GraphQLSchema({
     books{
       name
       category
+    }
+  }
+}
+------------------------------------------
+{
+  trainer(id:1){
+    name
+    courses{
+      id
+      title
+      likes
     }
   }
 }
